68a6bd7afd12967b36c0f44b5aa34a739cbc1f72
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2025-04-09 22:09:43 +0200

    0.0.16
    Signed-off-by: Manuel Daniel Dahmen <manuel.dahmen@gmx.com>

--- src/main/java/one/empty3/libs/Color.java
+++ src/main/java/one/empty3/libs/Color.java
@@ -30,6 +30,15 @@ public class Color extends java.awt.Color implements IColorMp {
         return this.getRGB()& 0x00FFFFFF;
     }
 
+    public int getRgb() {
+        int red = (int) (red() * 255f);
+        int green = (int) (green() * 255);
+        int blue = (int) (blue() * 255);
+        int color = ((red << 16) | (green << 8) | blue);;
+        return color|0xFF000000;
+    }
+
+
     public static Color newCol(float r1, float r2, float r3) {
         java.awt.Color color = new java.awt.Color(r1, r2, r3);
         return new Color(color.getRGB());
